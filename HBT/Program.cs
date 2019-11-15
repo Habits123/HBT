b0af5dc6cda842267b67c9656d8bbe7e94e50d7b
kim ngân <nganbuinguyen123@gmail.com>
2019-11-15 10:09:15 +0700

    Nhom HBT - Bui Nguyen Kim Ngan, Truong Vo Khoi

---
+++ HBT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BAI TAP 2 - Nhom HBT: https://github.com/Habits123/HBT
+        }
+    }
+}
